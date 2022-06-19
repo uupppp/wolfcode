94fe9e2017455330f3bd6e5dc352188442add106
Eastern unbeaten <chenshiyun2011@163.com>
2022-06-19 23:43:17 +0800

    处理 freemaker 的时间格式问题

--- web/src/main/java/cn/wolfcode/web/commons/config/JacksonConfig.java
+++ web/src/main/java/cn/wolfcode/web/commons/config/JacksonConfig.java
@@ -11,10 +11,7 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
-import freemarker.template.DefaultObjectWrapper;
-import freemarker.template.SimpleDate;
-import freemarker.template.TemplateModel;
-import freemarker.template.TemplateModelException;
+import freemarker.template.*;
 import link.ahsj.core.constants.TimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -40,20 +37,20 @@ public class JacksonConfig {
 
     @Autowired
     public void configureFreemarker(freemarker.template.Configuration configuration) {
-        configuration.setObjectWrapper (new DefaultObjectWrapper (freemarker.template.Configuration.VERSION_2_3_28) {
+        configuration.setObjectWrapper(new DefaultObjectWrapper(freemarker.template.Configuration.VERSION_2_3_28) {
 
             @Override
             public TemplateModel wrap(Object object) throws TemplateModelException {
                 if (object instanceof LocalDate) {
-                    return new SimpleDate (Date.valueOf ((LocalDate) object));
+                    return new SimpleScalar(DateTimeFormatter.ofPattern(TimeFormat.DEFAULT_DATE_FORMAT).format(((LocalDate) object)));
                 }
                 if (object instanceof LocalTime) {
-                    return new SimpleDate (Time.valueOf ((LocalTime) object));
+                    return new SimpleScalar(DateTimeFormatter.ofPattern(TimeFormat.DEFAULT_TIME_FORMAT).format(((LocalTime) object)));
                 }
                 if (object instanceof LocalDateTime) {
-                    return new SimpleDate (Timestamp.valueOf ((LocalDateTime) object));
+                    return new SimpleScalar(DateTimeFormatter.ofPattern(TimeFormat.DEFAULT_DATE_TIME_FORMAT).format(((LocalDateTime) object)));
                 }
-                return super.wrap (object);
+                return super.wrap(object);
             }
 
         });
@@ -61,20 +58,20 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper ( );
-        objectMapper.setDateFormat (new SimpleDateFormat (TimeFormat.DEFAULT_DATE_TIME_FORMAT));
-        objectMapper.disable (SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        objectMapper.disable (DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat(TimeFormat.DEFAULT_DATE_TIME_FORMAT));
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
         //反序列化时忽略json中多余的字段
-        objectMapper.configure (DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        JavaTimeModule javaTimeModule = new JavaTimeModule ( );
-        javaTimeModule.addSerializer (LocalDateTime.class, new LocalDateTimeSerializer (DateTimeFormatter.ofPattern (TimeFormat.DEFAULT_DATE_TIME_FORMAT)));
-        javaTimeModule.addSerializer (LocalDate.class, new LocalDateSerializer (DateTimeFormatter.ofPattern (TimeFormat.DEFAULT_DATE_FORMAT)));
-        javaTimeModule.addSerializer (LocalTime.class, new LocalTimeSerializer (DateTimeFormatter.ofPattern (TimeFormat.DEFAULT_TIME_FORMAT)));
-        javaTimeModule.addDeserializer (LocalDateTime.class, new LocalDateTimeDeserializer (DateTimeFormatter.ofPattern (TimeFormat.DEFAULT_DATE_TIME_FORMAT)));
-        javaTimeModule.addDeserializer (LocalDate.class, new LocalDateDeserializer (DateTimeFormatter.ofPattern (TimeFormat.DEFAULT_DATE_FORMAT)));
-        javaTimeModule.addDeserializer (LocalTime.class, new LocalTimeDeserializer (DateTimeFormatter.ofPattern (TimeFormat.DEFAULT_TIME_FORMAT)));
-        objectMapper.registerModule (javaTimeModule).registerModule (new ParameterNamesModule ( ));
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(TimeFormat.DEFAULT_DATE_TIME_FORMAT)));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(TimeFormat.DEFAULT_DATE_FORMAT)));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(TimeFormat.DEFAULT_TIME_FORMAT)));
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(TimeFormat.DEFAULT_DATE_TIME_FORMAT)));
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(TimeFormat.DEFAULT_DATE_FORMAT)));
+        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(TimeFormat.DEFAULT_TIME_FORMAT)));
+        objectMapper.registerModule(javaTimeModule).registerModule(new ParameterNamesModule());
         return objectMapper;
     }
 
